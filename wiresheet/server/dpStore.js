0569517c17442ccf652b83d06b7d0ff97962c30c
Gacontrol <Christoph.muff@ga-control.ch>
2026-04-08 20:45:16 +0200

    Updated dpStore.js

--- wiresheet/server/dpStore.js
+++ wiresheet/server/dpStore.js
@@ -129,7 +129,6 @@ function setFromLogicOutput(nodeValues) {
   for (const [dpKey, value] of Object.entries(nodeValues)) {
     if (dpKey.includes(':cfg:')) continue;
     if (dpKey.includes(':input-')) continue;
-    if (!dpKey.includes(':')) continue;
     const prev = _store.get(dpKey);
     if (prev !== value) {
       _store.set(dpKey, value);
